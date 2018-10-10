87cd976a2fe9a7324a8fde8f311b8deba856cb71
Arnbjörg Frímannsdóttir <arnbjorgfrimannsdottir@Arnbjorgs-MacBook-Air.local>
2018-10-10 17:49:07 +0000

    test for 1 integer

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,5 +1,15 @@
 const add = require("./calculator"); //import our greeting module
+ /*
+ it("should return ...", () => {
+	expect(add("")).toBe();
+});
+*/
+
 
 it("should return zero on an empty string", () => {
 	expect(add("")).toBe(0);
+});
+
+ it("should return the number when only one number is in the string", () => {
+	expect(add("5")).toBe(5);
 });
